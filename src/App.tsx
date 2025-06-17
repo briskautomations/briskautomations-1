827c8db9075d47a2d17a3edb6086122324c6ba21
briskautomations <briskautomationshq@gmail.com>
2025-06-17 00:59:44 -0400

    Create Cassie B. and Caleb B. Product Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BriskAutomationLanding from '@/components/ui/brisk-automation-landing';
 import TermsOfService from '@/components/pages/terms-of-service';
 import PrivacyPolicy from '@/components/pages/privacy-policy';
+import CassieBPage from '@/components/pages/cassie-b';
+import CalebBPage from '@/components/pages/caleb-b';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
           <Route path="/" element={<BriskAutomationLanding />} />
           <Route path="/terms" element={<TermsOfService />} />
           <Route path="/privacy" element={<PrivacyPolicy />} />
+          <Route path="/cassie-b" element={<CassieBPage />} />
+          <Route path="/caleb-b" element={<CalebBPage />} />
         </Routes>
       </div>
     </Router>
